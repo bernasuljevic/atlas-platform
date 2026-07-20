816b0e1fae8f87a96266a93f41b6253e197c6807
Berna Suljevic <bernasuljevic8@gmail.com>
2026-07-20 15:30:41 +0300

    Wiki GET guvenlik iyilestirmesi: giris yapilmamis isteklerde departman filtresi sessizce yok sayiliyor - DepartmentOnly sayfalar artik token'siz gorunmuyor

--- src/Modules/Wiki/Atlas.Modules.Wiki.Application/WikiPages/Queries/GetWikiPagesQueryHandler.cs
+++ src/Modules/Wiki/Atlas.Modules.Wiki.Application/WikiPages/Queries/GetWikiPagesQueryHandler.cs
@@ -1,4 +1,5 @@
 using Atlas.Modules.Wiki.Application.Abstractions;
+using Atlas.Shared.Contracts;
 using MediatR;
 
 namespace Atlas.Modules.Wiki.Application.WikiPages.Queries;
@@ -6,19 +7,25 @@ namespace Atlas.Modules.Wiki.Application.WikiPages.Queries;
 public class GetWikiPagesQueryHandler : IRequestHandler<GetWikiPagesQuery, IReadOnlyList<WikiPageDto>>
 {
     private readonly IWikiPageRepository _wikiPageRepository;
+    private readonly ICurrentUserAccessor _currentUser;
 
-    public GetWikiPagesQueryHandler(IWikiPageRepository wikiPageRepository)
+    public GetWikiPagesQueryHandler(IWikiPageRepository wikiPageRepository, ICurrentUserAccessor currentUser)
     {
         _wikiPageRepository = wikiPageRepository;
+        _currentUser = currentUser;
     }
 
     public async Task<IReadOnlyList<WikiPageDto>> Handle(GetWikiPagesQuery request, CancellationToken cancellationToken)
     {
         var allPages = await _wikiPageRepository.GetAllAsync(cancellationToken);
 
-        // Yetkilendirme filtresi: Domain'deki IsVisibleTo() kuralını burada çağırıyoruz.
-        // Handler "nasıl karar verileceğini" bilmiyor, sadece Domain'e soruyor.
-        var visiblePages = allPages.Where(p => p.IsVisibleTo(request.ViewerDepartmentName));
+        // Giriş yapmamış bir kullanıcı departman parametresi gönderse bile,
+        // bunu SESSİZCE yok sayıyoruz - hata fırlatmıyoruz, sadece filtreyi
+        // devre dışı bırakıyoruz. Sonuç: giriş yapmamış herkes sadece Public
+        // sayfaları görür, departman adını tahmin etmek işe yaramaz.
+        var effectiveDepartment = _currentUser.IsAuthenticated ? request.ViewerDepartmentName : null;
+
+        var visiblePages = allPages.Where(p => p.IsVisibleTo(effectiveDepartment));
 
         return visiblePages
             .Select(p => new WikiPageDto(
