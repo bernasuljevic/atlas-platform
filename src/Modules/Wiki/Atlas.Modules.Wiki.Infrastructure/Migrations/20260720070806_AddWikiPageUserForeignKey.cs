ddb9e1a3cbeaddf059aea5962bb13619b839623f
Berna Suljevic <bernasuljevic8@gmail.com>
2026-07-20 10:18:41 +0300

    wiki.WikiPages.CreatedByUserId icin auth.Users'a foreign key eklendi (raw SQL migration)

---
+++ src/Modules/Wiki/Atlas.Modules.Wiki.Infrastructure/Migrations/20260720070806_AddWikiPageUserForeignKey.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Atlas.Modules.Wiki.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWikiPageUserForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Wiki.Domain, Auth.Domain'i tanimadigi icin EF Core burada otomatik bir
+            // iliski kuramiyor - kisitlamayi ham SQL ile ekliyoruz. Bu, mimarimizin
+            // "modul kod seviyesinde ayrik, veritabani seviyesinde tutarli" ilkesinin
+            // somut karsiligi.
+            migrationBuilder.Sql(@"
+                ALTER TABLE [wiki].[WikiPages]
+                ADD CONSTRAINT [FK_WikiPages_Users_CreatedByUserId]
+                FOREIGN KEY ([CreatedByUserId])
+                REFERENCES [auth].[Users] ([Id]);
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Down() - migration geri alinirsa (ef migrations remove / database update
+            // ile eski bir migration'a donulurse) kisitlamayi kaldiran ters islem.
+            migrationBuilder.Sql(@"
+                ALTER TABLE [wiki].[WikiPages]
+                DROP CONSTRAINT [FK_WikiPages_Users_CreatedByUserId];
+            ");
+        }
+    }
+}
